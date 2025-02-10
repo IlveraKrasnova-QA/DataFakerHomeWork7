4255fbddbe6d79b600c370c09518b57340e5ef2a
krasnova.ilvera <ilvera.krasnova@yandex.ru>
2025-02-10 03:09:16 +0300

    fixes4

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -9,7 +9,7 @@ public class TestBase {
     static void beforeAll(){
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1928x1080";
-        Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.pageLoadStrategy = "eager";
     }
 }
